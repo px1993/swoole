171209c35a98b9d646432598844ba60ee73180b8
panxin <px13580379044@163.com>
2018-11-24 19:25:16 +0800

    修改

--- server/tcp.php
+++ server/tcp.php
@@ -19,19 +19,19 @@ $server->set([
 ]);
 
 //监听连接进入事件
-$serv->on('connect', function ($serv, $fd, $reactor_id) {
-    echo "Client: {$reactor_id} -- {$fd} Connect.\n";
+$server->on('connect', function ($serv, $fd, $reactor_id) {
+    echo "Server: {$serv} \t Client: {$reactor_id} -- {$fd} Connect.\n";
 });
 
 //监听数据接收事件
-$serv->on('receive', function ($serv, $fd, $from_id, $data) {
-    $serv->send($fd, "Server: {$serv} \n Client: {$fd} \n ReactorId: {$from_id}".$data);
+$server->on('receive', function ($serv, $fd, $from_id, $data) {
+    $serv->send($fd, "Server: {$serv} \t Client: {$from_id} -- {$fd} \n".$data);
 });
 
 //监听连接关闭事件
-$serv->on('close', function ($serv, $fd) {
-    echo "Client: {$fd} Close.\n";
+$server->on('close', function ($serv, $fd) {
+    echo "Server: {$serv} \t Client: {$fd} Close.\n";
 });
 
 //启动服务器
-$serv->start();
+$server->start();
